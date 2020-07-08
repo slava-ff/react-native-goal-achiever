e55b8235a74dc404e8c3813060587202ff00d5ac
s.famin <s.famin@godeltech.com>
2020-07-09 02:30:33 +0300

    feat: add button ADD

---
+++ src/components/AddButton.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
+
+const AddButton = () => {
+  const handleOnPressButton = () => {
+    console.log('====> add button click!');
+  };
+
+  return (
+    <View style={styles.wrapper}>
+      <TouchableOpacity style={styles.round} onPress={handleOnPressButton}>
+        <View style={styles.vertical} />
+        <View style={styles.horizontal} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    zIndex: 2,
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 30,
+    right: 50,
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+  },
+  round: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#00ADC1',
+    borderRadius: 50,
+  },
+  vertical: {
+    alignSelf: 'center',
+    marginTop: 20,
+    backgroundColor: 'white',
+    height: 40,
+    width: 10,
+    borderRadius: 4,
+    position: 'absolute',
+    opacity: 1,
+  },
+  horizontal: {
+    alignSelf: 'center',
+    marginTop: 35,
+    backgroundColor: 'white',
+    height: 10,
+    width: 40,
+    borderRadius: 4,
+    position: 'absolute',
+    opacity: 1,
+  },
+});
+
+export default AddButton;
--- src/pages/TopLevelGoals.js
+++ src/pages/TopLevelGoals.js
@@ -1,43 +1,82 @@
 import React from 'react';
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   ScrollView,
-//   View,
-//   Text,
-//   StatusBar,
-// } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-// import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 // import GoalLine from '../components/GoalLine';
 import GoalFlatList from '../components/GoalFlatList';
+import AddButton from '../components/AddButton';
 import goalsTemp from '../helpers/data.example';
 
 const TopLevelGoals = ({navigation}) => {
   return (
     <>
+      {/* <View style={{position: 'relative'}}> */}
       {/* <StatusBar barStyle="dark-content" /> */}
-      {/* <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        />
-        <GoalLine />
-        <GoalLine />
-        <GoalLine />
-        <GoalLine />
-        <GoalLine />
-      </SafeAreaView> */}
-      <GoalFlatList goalsTemp={goalsTemp} navigation={navigation} />
+      {/* <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}>
+        <View
+          style={{
+            borderColor: 'black',
+            margin: 30,
+            borderWidth: 2,
+            height: 140,
+          }}>
+          <Text>Aasasdasdasdsd</Text>
+        </View>
+        <View
+          style={{
+            borderColor: 'black',
+            margin: 30,
+            borderWidth: 2,
+            height: 140,
+          }}>
+          <Text>Aasasdasdasdsd</Text>
+        </View>
+        <View
+          style={{
+            borderColor: 'black',
+            margin: 30,
+            borderWidth: 2,
+            height: 140,
+          }}>
+          <Text>Aasasdasdasdsd</Text>
+        </View>
+        <View
+          style={{
+            borderColor: 'black',
+            margin: 30,
+            borderWidth: 2,
+            height: 140,
+          }}>
+          <Text>Aasasdasdasdsd</Text>
+        </View>
+      </ScrollView> */}
+      <GoalFlatList
+        goalsTemp={goalsTemp}
+        navigation={navigation}
+        style={styles.flatList}
+      />
+      <AddButton style={styles.btn} />
+      {/* </View> */}
     </>
   );
 };
 
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-// });
+const styles = StyleSheet.create({
+  // scrollView: {
+  //   backgroundColor: Colors.lighter,
+  // },
+  flatList: {},
+  btn: {},
+});
 
 export default TopLevelGoals;
